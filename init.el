bcb69a2a53df0faf3e8bb35e826f6e7ff9d8a711
Kuba <kt@kuba.zf>
2016-10-13 20:20:07 -0400

    ss

--- init.el
+++ init.el
@@ -1,7 +1,9 @@
-(server-start)
+;(server-start)
 (package-initialize)
 
 (global-set-key [f10] '(lambda () (interactive) (switch-to-buffer "*scratch*")))
+(global-set-key [f9] 'hl-line-mode)
+(global-set-key [f8] 'crosshairs-mode)
 
 (global-set-key (kbd "C-x C-b") 'ibuffer) 
 (global-set-key (kbd "RET") 'newline-and-indent)
@@ -10,16 +12,85 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
+(global-set-key (kbd "C-x p") 'previous-multiframe-window)
+
+(global-unset-key (kbd "C-w"))
+(global-set-key (kbd "C-w") 'backward-kill-word)
+
+(global-set-key (kbd "C-c w n") 'wconf-use-next)
+(global-set-key (kbd "C-c w p") 'wconf-use-previous)
+(global-set-key (kbd "C-c w s") 'wconf-store-all)
+(global-set-key (kbd "C-c w l") 'wconf-restore-all)
+(global-set-key (kbd "C-c w w") 'wconf-save)
+
+;(global-unset-key (kbd "C-^"))
+;(global-set-key (kbd "C-^") 'goto-match-paren)
+(global-set-key (kbd "C-^") 'forward-or-backward-sexp)
+
+(global-unset-key (kbd "ESC <up>"))
+
+(global-set-key (kbd "ESC <down>") 'scroll-up-line)
+(global-set-key (kbd "ESC <up>") 'scroll-down-line)
+
+(global-set-key (kbd "<C-M-down>") 'scroll-other-window-up)
+(global-set-key (kbd "<C-M-up>") 'scroll-other-window-down)
+
 (global-set-key "\M-y" 'popup-kill-ring)
                                         ;(global-set-key (kbd "C-x g") 'google-this-mode-submap)
 
 (hl-line-mode)
 
-(display-time)
-(defalias 'list-buffers 'ibuffer)
-(desktop-save-mode t)
-(show-paren-mode t)
-(follow-mode t)
+(defun scroll-other-window-down ()
+  (interactive)
+  (scroll-other-window -1)
+  )
+
+(defun scroll-other-window-up ()
+  (interactive)
+  (scroll-other-window 1)
+  )
+
+
+;(defun pbcopy ()
+;  (interactive)
+;  (call-process-region (point) (mark) "pbcopy")
+;  (setq deactivate-mark t))
+;
+;(defun pbpaste ()
+;  (interactive)
+;  (call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t))
+;
+;(defun pbcut ()
+;  (interactive)
+;  (pbcopy)
+;  (delete-region (region-beginning) (region-end)))
+;
+;(global-set-key (kbd "C-c c") 'pbcopy)
+;(global-set-key (kbd "C-c v") 'pbpaste)
+;(global-set-key (kbd "C-c x") 'pbcut)
+;
+
+(set-face-background hl-line-face "color-235")
+(set-face-background col-highlight-face "color-235")
+
+(setq toggle-crosshairs-when-idle 3)
+
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis, otherwise insert %.
+vi style of % jumping to matching brace."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+
+(defun forward-or-backward-sexp (&optional arg)
+  "Go to the matching parenthesis character if one is adjacent to point."
+  (interactive "p")
+  (cond ((looking-at "\\s(") (forward-sexp arg))
+        ((looking-back "\\s)" 1) (backward-sexp arg))
+        ;; Now, try to succeed from inside of a bracket
+        ((looking-at "\\s)") (forward-char) (backward-sexp arg))
+        ((looking-back "\\s(" 1) (backward-char) (forward-sexp arg))))
 
 
 (autoload 'ruby-mode "ruby-mode" "Ruby Mode." t)
@@ -27,9 +98,23 @@
 (require 'package)
 (require 'tramp)
 
+(setq tramp-default-method "scp")
 
-(add-to-list 'tramp-default-method-alist '("192.168.4.44" "" "telnet"))
+(add-to-list 'tramp-connection-properties
+             (list (regexp-quote "/ssh:mac:")
+                   "remote-shell" "/bin/bash"))
+
+(add-to-list 'tramp-connection-properties
+             (list (regexp-quote "/ssh:mac:")
+                   "remote-shell-login" "/bin/bash"))
+
+(add-to-list 'tramp-default-method-alist '("mac" "" "scp"))
+(add-to-list 'tramp-default-method-alist '("192.168.59.3" "" "scp"))
 (add-to-list 'tramp-default-method-alist '("192.168.4.100" "" "ftp"))
+
+(setq desktop-buffers-not-to-save "^$")
+
+
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
 (add-to-list
  'package-archives
@@ -63,8 +148,6 @@
 
 
 
-
-
 (defvar kt/packages '(ac-slime
                           auto-complete
                           autopair
@@ -94,7 +177,7 @@
                           php-mode
                           puppet-mode
                           restclient
-                          rvm
+b                          rvm
                           scala-mode
                           smex
                           shell-switcher
@@ -171,11 +254,30 @@
 
 
 (hackernews)
+;(set-face-attribute 'default nil :family "Literation Mono Powerline")
+;(set-face-attribute 'default nil :family "Droid Sans Mono Slashed Powerline")
 
+(set-face-attribute 'default nil :font "Inconsolata for Powerline")
+;(set-face-attribute 'default nil :family "Meslo LG S for Powerline")
+;(set-frame-font "Meslo LG S for Powerline")
+;(set-frame-font "Inconsolata-dz for Powerline")
+;(set-face-attribute 'default nil :font "Roboto Mono Light for Powerline")
+;(set-face-attribute 'default nil :family "Andale Mono")
+;(set-face-attribute 'default nil :family "Monaco")
+
+
+(set-face-attribute 'default nil :height 120)
 
 (setq my-timer (run-with-idle-timer 240 1 'refresh-hackernews))
 (setq tramp-default-method "ssh")
+(setq temporary-file-directory "/tmp/")
+(setq small-temporary-file-directory "/tmp/")
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq kill-whole-line t)
+(setq show-paren-style 'expression)
 
+(show-paren-mode t)
 
 ;(require 'spaceline-config)
 ;(spaceline-spacemacs-theme)
@@ -201,6 +303,7 @@
     (regexp-quote " ") "-" (current-time-string))))
 
 (defun save-persistent-scratch ()
+  (interactive)
   "write the contents of *scratch* to the file name persistent-scratch-filename, making a backup copy in persistent-scratch-backup-directory"
   (with-current-buffer (get-buffer "*scratch*")
     (if (file-exists-p persistent-scratch-filename)
@@ -223,6 +326,16 @@
       (with-current-buffer (get-buffer "*scratch*")
         (delete-region (point-min) (point-max))
         (shell-command (format "cat %s" persistent-scratch-filename) (current-buffer)))))
+
+(defun scratch-remap-save-key ()
+  (with-current-buffer (get-buffer "*scratch*")
+    (setq scratchmap (make-sparse-keymap))
+    (use-local-map scratchmap)
+    (local-set-key (kbd "C-x C-s") 'save-persistent-scratch)))
+
+
+(scratch-remap-save-key)
+
 
 (load-persistent-scratch)
 
@@ -283,7 +396,7 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 
 (engine-mode)
 
-(define-key haskell-mode-map "\C-ch" 'haskell-hoogle)
+;(define-key haskell-mode-map "\C-ch" 'haskell-hoogle)
 
 (defengine google
   "https://www.google.com/#q=%s"
@@ -320,21 +433,29 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 
 (setq haskell-hoogle-command "/Users/kt/.local/bin/hoogle")
 
-(do-applescript "tell application \"Google Chrome\"
-	activate
-	set active tab index of first window to 1
-	
-	tell application \"System Events\" to keystroke \"/\"
-end tell")
+;(do-applescript "tell application \"Google Chrome\"
+;	activate
+;	set active tab index of first window to 1
+;	
+;	tell application \"System Events\" to keystroke \"/\"
+;end tell")
+;
+;(defun gmail-search (s)
+;  (interactive "sEnter query: ")
+;  
+;  (do-applescript (format "tell application \"Google Chrome\" to open location \"https://mail.google.com/mail/u/0/#search/%s\"" s)))
+;(global-set-key (kbd "C-x m") 'gmail-search)
+;
 
-(defun gmail-search (s)
-  (interactive "sEnter query: ")
-  
-  (do-applescript (format "tell application \"Google Chrome\" to open location \"https://mail.google.com/mail/u/0/#search/%s\"" s)))
-(global-set-key (kbd "C-x m") 'gmail-search)
+(require 'wconf)
 
+(require 'mouse)
 
+(xterm-mouse-mode t)
 
+(defun track-mouse (e))
+
+(setq mouse-sel-mode t)
 
 ;dired+
 
@@ -360,3 +481,24 @@ end tell")
 
 (add-hook 'after-init-hook (lambda () (load-theme 'zenburn t)))
 
+
+(add-hook 'after-init-hook (lambda () (wconf-load "/home/kt/.emacs.d/wconf-window-config.el")))
+
+
+
+(load-theme 'zenburn t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (multishell multi-eshell eshell-git-prompt yaml-mode json-mode wconf window-layout layout-restore zenburn-theme typing spaceline smex shell-switcher rvm ruby-block popup-kill-ring osx-browse org magit-tramp magit-annex intero ibuffer-git helm-hoogle hackernews google-this find-file-in-repository engine-mode dropbox docker-api docker dired+ crosshairs command-t bundler btc-ticker browse-kill-ring autopair auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
